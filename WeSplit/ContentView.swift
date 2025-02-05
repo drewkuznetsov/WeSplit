f168ecb2971c990dfbb6cdff9a0e548c09fc76be
Andrey Kuznetsov <8150309@gmail.com>
2025-02-05 20:35:22 +0300

    Buton Commit

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var tapCount = 0
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Button("Tap Count: \(tapCount)") {
+            self.tapCount += 1
         }
-        .padding()
     }
 }
 
